f72933a474dcdd4b8b948855126ad94f8c7c8d93
Afton Downey and Bri Popson <aaike00@gmail.com>
2016-03-03 08:55:42 -0800

    start creating book and author classes

--- src/Author.php
+++ src/Author.php
@@ -19,6 +19,65 @@
         {
             return $this->name;
         }
+
+        function setId($id)
+        {
+            $this->id = $id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+          $GLOBALS['DB']->exec("INSERT INTO authors (name) VALUES ('{$this->getName()}');");
+          $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_authors = $GLOBALS['DB']->query("SELECT * FROM authors;");
+            $authors = array();
+            foreach($returned_authors as $author) {
+                $name = $author['name'];
+                $id = $author['id'];
+                $new_author = new Author($name, $id);
+                array_push($authors, $new_author);
+            }
+            return $authors;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM authors;");
+        }
+
+        function deleteAuthor()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM authors WHERE id-={$this->getId()};");
+        }
+
+        function findAuthor($search_id)
+        {
+            $found_author = null;
+            $authors = Author::getAll();
+            foreach($authors as $author) {
+                $author_id = $author->getId();
+                if ($author_id == $search_id) {
+                    $found_author = $author;
+                }
+            }
+            return $found_author;
+        }
+
+        function updateAuthor($new_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE authors SET name = '{$new_name}' WHERE id = {$this->getId()};");
+            $this->setName($new_name);
+        }
+
     }
 
 
--- src/Book.php
+++ src/Book.php
@@ -1,12 +1,77 @@
 <?php
-class WHATEVER_CLASS 
+class Book
 {
-    private
+    private $title;
+    private $id;
 
 
-    function __construct()
+    function __construct($title, $id = null)
     {
+      $this->title = $title;
+      $this->id = $id;
+    }
 
+    function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    function getTitle()
+    {
+        return $this->title;
+    }
+
+    function getId()
+      {
+          return $this->id;
+      }
+
+    function save()
+    {
+        $GLOBALS['DB']->exec("INSERT INTO books (title) VALUES ('{$this->getTitle()}');");
+        $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    static function getAll()
+    {
+        $returned_books = $GLOBALS['DB']->query("SELECT * FROM books;");
+        $books = [];
+        foreach($returned_books as $book) {
+            $title = $book['title'];
+            $id = $book['id'];
+            $new_book = new Book($title, $id);
+            array_push($books, $new_book);
+        }
+        return $books;
+    }
+
+    static function deleteAll()
+    {
+      $GLOBALS['DB']->exec("DELETE FROM books;");
+    }
+
+    function updateBook($new_title)
+    {
+        $GLOBALS['DB']->exec("UPDATE books SET title = '{$new_title}' WHERE id = {$this->getId()};");
+        $this->setTitle($new_title);
+    }
+
+    function deleteBook()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM books WHERE id = {$this->getId()};");
+    }
+
+    static function findBook($search_id)
+    {
+        $found_book = null;
+        $books = Book::getAll();
+        foreach($books as $book) {
+            $book_id = $book->getId();
+            if ($book_id == $search_id) {
+              $found_book = $book;
+            }
+        }
+        return $found_book;
     }
 
 
